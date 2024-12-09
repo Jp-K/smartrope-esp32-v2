d30b3c857d80d85121a086eaf4bd1d3d2a774b3d
João Paulo Kraisch <joaopaulokraisch@gmail.com>
2024-12-09 19:14:16 -0300

    params

---
+++ Firmware/include/MQTTParams.h
@@ -0,0 +1,19 @@
+// This file was auto generated, do not modify it directly,
+// modify the original file instead:
+//
+// pre-processed/include/MQTTParams.h
+
+#ifndef ROPE_MQTT_PARAMS
+#define ROPE_MQTT_PARAMS
+
+#define MQTT_URL "http://smartrope.com.br:1883"
+#define MQTT_CLIENT_ID "RoPE 19030001"
+
+#define MQTT_CONTROL_TOPIC "control"
+#define MQTT_EVENTS_TOPIC "rope/19030001/events"
+
+#define MQTT_IDENTITY_TOPIC "rope/19030001/identity"
+#define MQTT_STATUS_TOPIC "rope/19030001/status"
+#define MQTT_OFFLINE_PAYLOAD "{\"status\":\"OFFLINE\"}"
+
+#endif // ROPE_MQTT_PARAMS
---
+++ Firmware/include/WiFiParams.h
@@ -0,0 +1,19 @@
+// This file was auto generated, do not modify it directly,
+// modify the original file instead:
+//
+// pre-processed/include/WiFiParams.h
+
+#ifndef ROPE_WIFI_PARAMS
+#define ROPE_WIFI_PARAMS
+
+#define WIFI_SSID "Smart-RoPE"
+#define WIFI_PASSWORD "smartrope54321"
+#define WIFI_HOSTNAME "rope-19030001"
+
+// #define WIFI_SSID "noschang-note"
+// #define WIFI_PASSWORD "30121988"
+// #define WIFI_HOSTNAME "rope-19030001"
+
+
+
+#endif // ROPE_WIFI_PARAMS
